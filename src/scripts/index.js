c063bf89c4e9bd788cd99711b70642b97d72a70a
Kusnadi34 <nanakusnadi035@gmail.com>
2026-04-25 15:43:15 +0700

    Simplify service worker registration

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,9 +4,7 @@ import App from './pages/app.js';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      console.log('SW registered:', reg);
-    }).catch(err => console.log('SW failed:', err));
+    navigator.serviceWorker.register('./sw.js');
   });
 }
 
